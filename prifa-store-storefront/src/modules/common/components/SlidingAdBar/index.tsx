de922c9421735bff692fe9f711324705308f4747
AhmetKattaa <ahmadkattaa2020@gmail.com>
2024-10-19 20:40:19 +0300

    current month in adbar

--- prifa-store-storefront/src/modules/common/components/SlidingAdBar/index.tsx
+++ prifa-store-storefront/src/modules/common/components/SlidingAdBar/index.tsx
@@ -2,18 +2,24 @@ import React from 'react';
 import './SlidingAdBar.css'; // CSS dosyasını bağlıyoruz
 
 const SlidingAdBar = () => {
+  const monthNames = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+  const currentDate = new Date();
+  const currentMonthName = monthNames[currentDate.getMonth()];
   return (
     <div className="sliding-adbar-container text-white mb-10">
       <div className="sliding-content">
         {/* Aynı içeriklerin iki kez tekrarlandığı alan */}
         <div className="ad-set">
-          <span>🔥 Limited to SEPTEMBER Only 🔥</span>
+          <span>🔥 Limited to {currentMonthName} Only 🔥</span>
           <span>⚡️ Free Shipping ⚡️</span>
           <span>🚚 30 Day Returns 🚚</span>
           <span>💥 45% OFF all options 💥</span>
         </div>
         <div className="ad-set">
-          <span>🔥 Limited to SEPTEMBER Only 🔥</span>
+          <span>🔥 Limited to {currentMonthName} Only 🔥</span>
           <span>⚡️ Free Shipping ⚡️</span>
           <span>🚚 30 Day Returns 🚚</span>
           <span>💥 45% OFF all options 💥</span>
